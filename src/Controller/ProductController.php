c37dac8b69a5863720ec25e4dabb1189123e1c57
Alexis Coinet <alexis.coinet@ynov.com>
2020-12-16 10:33:31 +0100

    change newproduct template

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -11,29 +11,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProductController extends AbstractController
 {
     /**
-     * @Route("/product", name="product")
+     * @Route("/newproduct", name="new_product")
      */
     public function index(): Response
     {
         $product = new Product();
-        $form = $this->createForm(ProductType::class, $product);
-        if ($form->isSubmitted() && $form->isValid()) {
-            // $form->getData() holds the submitted values
-            // but, the original `$task` variable has also been updated
-            $product = $form->getData();
-
-            // ... perform some action, such as saving the task to the database
-            // for example, if Task is a Doctrine entity, save it!
-            // $entityManager = $this->getDoctrine()->getManager();
-            // $entityManager->persist($task);
-            // $entityManager->flush();
-
-            return $this->redirectToRoute('task_success');
-        }
 
         return $this->render('product/addproduct.html.twig', [
             'controller_name' => 'ProductController',
-            'form' => $form->createView()
         ]);
     }
 }

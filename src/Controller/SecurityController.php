afe5d7e559eeaeaf6d28f720f214ecfb3ffd29b5
Lilian <lilianouvmmi@gmail.com>
2021-01-13 15:10:05 +0100

    responsive design

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -25,7 +25,7 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('login/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('login/login-2.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
     /**

073bd6d3e4b2e8207fe1ac05380e4f7e15332e6b
Alexis <alexiscoinet12@gmail.com>
2022-03-30 14:05:21 +0200

    change admin dashboard page

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Brand;
@@ -21,7 +22,7 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+        return $this->render('admin/dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
@@ -33,12 +34,17 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Tableau de bord', 'fa fa-home');
-        yield MenuItem::linktoRoute('Retour au site', 'fas fa-arrow-left', 'homepage');
+        yield MenuItem::linktoRoute('Retour au site', 'fas fa-arrow-left', 'accueil');
         yield MenuItem::linkToCrud('Marque', 'fas fa-star', Brand::class);
         yield MenuItem::linkToCrud('FAQ Question', 'fas fa-question', FAQQuestion::class);
         yield MenuItem::linkToCrud('FAQ Theme', 'fas fa-brush', FAQTheme::class);
         yield MenuItem::linkToCrud('Qualité', 'fas fa-medal', Quality::class);
         yield MenuItem::linkToCrud('Categorie', 'fas fa-box', Category::class);
         yield MenuItem::linkToCrud('Taille', 'fas fa-tshirt', Size::class);
+    }
+
+    public function configureAssets(): Assets
+    {
+        return Assets::new()->addCssFile('style/admin/admin.css');
     }
 }
